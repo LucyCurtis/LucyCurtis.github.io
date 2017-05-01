8538021c630c116db8a1a84917bf32e656ad9ff6
LucyCurtis <lucy@misslucy.internode.on.net>
2017-05-01 18:57:38 +1000

    three

---
+++ hover/assets/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  $('.number').mouseenter(function() {
+    $('.footnote').addClass('showme');
+  });
+
+
+  $('.number').mouseleave(function() {
+    $('.footnote').removeClass('showme');
+  });
+
+  $('.cloud').mouseenter(function() {
+  $('.metahaven').addClass('orange');
+});
+
+$('.cloud').mouseleave(function() {
+  $('.metahaven').removeClass('orange');
+});
+
+$('.thumbnail').click(function() {
+   $(this).toggleClass('enlarge');
+ });
+
+
+});
