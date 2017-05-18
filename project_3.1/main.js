86238655c0caebb10095e6d78f96276e09cdee50
LucyCurtis <lucy@misslucy.internode.on.net>
2017-05-18 22:31:23 +1000

    test

---
+++ project_3.1/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+$('.chan').mouseenter(function() {
+  $('.chan__image').addClass('showme');
+});
+
+
+
+$('.political').mouseenter(function() {
+  $('.political__image').addClass('showme');
+});
+
+
+
+$('.pepper').mouseenter(function() {
+  $('.pepper__image').addClass('showme');
+});
+
+//$('.pepper').mouseleave(function() {
+//  $('.pepper__image').removeClass('showme');
+//});
+
+$('.american').mouseenter(function() {
+  $('.american__image').addClass('showme');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
