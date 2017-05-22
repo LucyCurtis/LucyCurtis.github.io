b401a43a19be91c32d66caad70d5caf23ba5132b
LucyCurtis <lucy@dyn-118-139-43-198.its.monash.edu.au>
2017-05-23 08:50:28 +1000

    test

---
+++ project_3.1/assets/js/doge.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+$('.doge').mouseenter(function() {
+  $('.doge__image').addClass('showme');
+});
+
+$('.wow').mouseleave(function() {
+  $('.wow__image').removeClass('showme');
+});
+
+$('.wow').mouseenter(function() {
+  $('.wow__image').addClass('showme');
+});
+
+$('.such').mouseleave(function() {
+  $('.such__image').removeClass('showme');
+});
+
+$('.shiba').mouseenter(function() {
+  $('.shiba__image').addClass('showme');
+});
+
+$('.shiba').mouseleave(function() {
+  $('.shiba__image').removeClass('showme');
+});
+
+$('.citrus').mouseenter(function() {
+  $('.citrus__image').addClass('showme');
+});
+
+$('.citrus').mouseleave(function() {
+  $('.citrus__image').removeClass('showme');
+});
+
+$('.most').mouseenter(function() {
+  $('.most__image').addClass('showme');
+});
+
+$('.most').mouseleave(function() {
+  $('.most__image').removeClass('showme');
+});
+
+
+
+  });
