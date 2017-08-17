4dc964e20663fd32bd5159c1ba35cc8142b2fdb0
LucyCurtis <lucy@misslucy.internode.on.net>
2017-08-18 07:53:04 +1000

    pushed

---
+++ 5posters/level3.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+$('div').hide();
+
+   setTimeout(function(){
+   $('div.d').fadeIn('slow');},100);
+
+   setTimeout(function(){
+   $('div.e').fadeIn('slow');},200);
+
+   setTimeout(function(){
+   $('div.s').fadeIn('slow');},300);
+
+   setTimeout(function(){
+   $('div.i').fadeIn('slow');},400);
+
+   setTimeout(function(){
+   $('div.g').fadeIn('slow');},500);
+
+   setTimeout(function(){
+   $('div.n').fadeIn('slow');},600);
+
+
+
+  });
